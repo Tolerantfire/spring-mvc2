b0ab54f2423df3f3a95c89c20a740c4bd19a75f3
tolerantfire <andrewmkrtchan91@gmail.org>
2020-11-05 23:46:55 +0300

    v2

--- 2.2.2. spring_mvc/src/main/java/web/Model/Car.java
+++ 2.2.2. spring_mvc/src/main/java/web/Model/Car.java
@@ -1,31 +1,9 @@
-package Model;
+package web.Model;
 
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
 public class Car {
     private int version;
     private String model;
     private String color;
-
-    static Car car1 = new Car(1, "Skoda", "Black");
-    static Car car2 = new Car(2, "BMV", "Grey");
-    static Car car3 = new Car(3, "Lada", "Blue");
-    static Car car4 = new Car(4, "Toyota", "Red");
-    static Car car5 = new Car(5, "Renault", "Green");
-
-    static public List<Car> createList (){
-        List<Car> list = new ArrayList<>();
-        list.add(car1);
-        list.add(car2);
-        list.add(car3);
-        list.add(car4);
-        list.add(car5);
-        return list;
-    };
 
     public Car(int version, String model, String color) {
         this.version = version;
